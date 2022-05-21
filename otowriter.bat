61bae619f974bc0f7ff955581842473af05310a7
mr1ay <48817265+mr1ay@users.noreply.github.com>
2022-05-21 13:40:54 +0300

    Add files via upload

---
+++ otowriter.bat
@@ -0,0 +1,15 @@
+@echo off
+::edited=mr1ay
+::writed=21.05.2022
+timeout /t 5 
+set a=0 
+:loop
+set /a a=%a%+1
+nircmd clipboard set "set c%a%=0a " 
+nircmd sendkeypress ctrl+v
+nircmd sendkeypress enter
+::nircmd sendkeypress down
+ 
+if %a%==20 goto :End
+goto :loop
+:end
