01f6b9c2383dcbe414b84fa6b3dbe75646175c76
mr1ay <48817265+mr1ay@users.noreply.github.com>
2022-05-21 14:13:22 +0300

    Update otowriter.bat

--- otowriter.bat
+++ otowriter.bat
@@ -1,12 +1,12 @@
-       @echo off
-       ::edited=mr1ay
-       ::writed=21.05.2022
+        @echo off
+        ::edited=mr1ay
+        ::writed=21.05.2022
 	::version=1.0
        
-       timeout /t 5 
-       set a=0 
+        timeout /t 5 
+        set a=0 
        
-       :loop
+        :loop
               ::timeout /t 1 >nul
 	      
               set /a a=%a%+1
@@ -17,4 +17,4 @@
  
               if %a%==20 goto :End
               goto :loop
-       :end
+        :end
