713896a545e6fa19e5afee5a0adbc1cd9ae44e3f
mr1ay <48817265+mr1ay@users.noreply.github.com>
2022-05-21 14:03:06 +0300

    Update otowriter.bat

--- otowriter.bat
+++ otowriter.bat
@@ -1,15 +1,17 @@
-@echo off
-::edited=mr1ay
-::writed=21.05.2022
-timeout /t 5 
-set a=0 
-:loop
-set /a a=%a%+1
-nircmd clipboard set "set c%a%=0a " 
-nircmd sendkeypress ctrl+v
-nircmd sendkeypress enter
-::nircmd sendkeypress down
+       @echo off
+       ::edited=mr1ay
+       ::writed=21.05.2022
+       
+       timeout /t 5 
+       set a=0 
+       
+       :loop
+              set /a a=%a%+1
+              nircmd clipboard set "set c%a%=0a " 
+              nircmd sendkeypress ctrl+v
+              nircmd sendkeypress enter
+              ::nircmd sendkeypress down
  
-if %a%==20 goto :End
-goto :loop
-:end
+              if %a%==20 goto :End
+              goto :loop
+       :end
